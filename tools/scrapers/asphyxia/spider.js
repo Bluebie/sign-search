8184b11114f1f84c6936f47e78d1bb33ecac0d7d
Phoenix Fox <phx@amanita.lan>
2019-08-12 23:49:10 +1000

    Tweak Asphyxia scraper to prefer mp4 downloads, and add some more timing files

--- tools/scrapers/asphyxia/spider.js
+++ tools/scrapers/asphyxia/spider.js
@@ -84,7 +84,7 @@ class CachingYoutubeDownloaderSource {
     if (!await fs.pathExists(this.localFilename)) {
       return await (new Promise((resolve, reject) => {
         console.log(`!!! Downloading Youtube video at: ${this.localFilename}`)
-        let video = ytdl(this.url)
+        let video = ytdl(this.url, ['--format', 'bestvideo[ext=mp4]'])
         video.pipe(require('fs').createWriteStream(this.localFilename))
         video.on('end', ()=> { this.fetched = true; resolve(this.localFilename) })
         video.on('error', (e)=> reject(e))
