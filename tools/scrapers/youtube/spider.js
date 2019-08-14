7c2ed183974f7d5c736ab8038aaeb80a2b0fef69
Phoenix Fox <phx@Amanita.local>
2019-08-15 00:30:24 +1000

    More robustness to youtube scraper for parsing video titles in to words lists for embedding

--- tools/scrapers/youtube/spider.js
+++ tools/scrapers/youtube/spider.js
@@ -35,7 +35,7 @@ async function scanPlaylists(playlistURLs) {
       }
 
       // slice at the first : then use the last : section as a comma seperated list of words to mean average together to find vector
-      wordsList = wordsList.split(/:/).slice(-1)[0].split(',').map((x)=> x.trim())
+      wordsList = wordsList.split(/:/).slice(-1)[0].split(/[^a-zA-Z0-9'‘]+/).map((x)=> x.trim()).filter(x => x.match(/[a-zA-Z0-9]+/))
       
       signVideos.push({
         url: info.webpage_url,
