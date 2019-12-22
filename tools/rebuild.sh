bbf69df01d47fe686a04a4f60f702f4ba858f099
Phoenix Fox <Bluebie@users.noreply.github.com>
2019-12-22 20:36:15 +1100

    Edit to how logging is done

--- tools/rebuild.sh
+++ tools/rebuild.sh
@@ -1,13 +1,13 @@
 cd scrapers/asphyxia
-node spider.js
+node spider.js > ../logs/asphyxia.txt
 cd ../..
 cd scrapers/auslan-signbank
-node spider.js
+node spider.js > ../logs/auslan-signbank.txt
 cd ../..
 cd scrapers/auslan-stage-left
-node spider.js
+node spider.js > ../logs/auslan-stage-left.txt
 cd ../..
 cd scrapers/youtube
-node spider.js
+node spider.js > ../logs/youtube.txt
 cd ../..
-node create-datasets-torrent.js
+node create-datasets-torrent.js > ../logs/create-datasets-torrent.txt
