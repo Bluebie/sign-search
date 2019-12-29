5012c57d2bff9becc6c8b54850c34fa8c79c3a15
Phoenix Fox <Bluebie@users.noreply.github.com>
2019-12-29 23:15:14 +1100

    fix paths in rebuild script to fix logging of scrapers

--- tools/rebuild.sh
+++ tools/rebuild.sh
@@ -1,13 +1,13 @@
 cd scrapers/asphyxia
-node spider.js > ../logs/asphyxia.txt
+node spider.js > ../../../logs/asphyxia.txt
 cd ../..
 cd scrapers/auslan-signbank
-node spider.js > ../logs/auslan-signbank.txt
+node spider.js > ../../../logs/auslan-signbank.txt
 cd ../..
 cd scrapers/auslan-stage-left
-node spider.js > ../logs/auslan-stage-left.txt
+node spider.js > ../../../logs/auslan-stage-left.txt
 cd ../..
 cd scrapers/youtube
-node spider.js > ../logs/youtube.txt
+node spider.js > ../../../logs/youtube.txt
 cd ../..
 node create-datasets-torrent.js > ../logs/create-datasets-torrent.txt
