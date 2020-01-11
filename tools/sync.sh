6811abeae3027bdfe52fb8f7c0e9b66de7af8715
Phoenix Fox <Bluebie@users.noreply.github.com>
2020-01-12 01:48:49 +1100

    tools/sync.sh - backup feeds before sync

--- tools/sync.sh
+++ tools/sync.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rsync -avr --delete-after web@auslan.fyi:/home/web/beta.find.auslan.fyi/logs -e ssh ~/Sync/sign-search
 rsync -avr --delete-after web@auslan.fyi:/home/web/beta.find.auslan.fyi/datasets -e ssh ~/Sync/sign-search
+rsync -avr --delete-after web@auslan.fyi:/home/web/beta.find.auslan.fyi/feeds -e ssh ~/Sync/sign-search
 rsync -avr --delete-after web@auslan.fyi:/home/web/beta.find.auslan.fyi/datasets.torrent -e ssh ~/Sync/sign-search
 rsync -avr --delete-after web@auslan.fyi:/home/web/beta.find.auslan.fyi/tools/spiders/frozen-data -e ssh ~/Sync/sign-search/tools/spiders
 rsync -avr --delete-after --exclude=".DS_Store" --exclude="/node_modules" --exclude="/.git" --exclude="/.dat" --exclude="/datasets" ~/Sync/sign-search/* -e ssh web@auslan.fyi:/home/web/beta.find.auslan.fyi
