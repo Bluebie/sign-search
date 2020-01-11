f92a7be971bbbaf4bc6a86d53bffe0217eda7229
Phoenix Fox <Bluebie@users.noreply.github.com>
2020-01-11 15:43:20 +1100

    Update sync.sh

--- tools/sync.sh
+++ tools/sync.sh
@@ -3,4 +3,4 @@ rsync -avr --delete-after web@auslan.fyi:/home/web/beta.find.auslan.fyi/logs -e 
 rsync -avr --delete-after web@auslan.fyi:/home/web/beta.find.auslan.fyi/datasets -e ssh ~/Sync/sign-search
 rsync -avr --delete-after web@auslan.fyi:/home/web/beta.find.auslan.fyi/datasets.torrent -e ssh ~/Sync/sign-search
 rsync -avr --delete-after web@auslan.fyi:/home/web/beta.find.auslan.fyi/tools/spiders/frozen-data -e ssh ~/Sync/sign-search/tools/spiders
-rsync -avr --delete-after --exclude="/node_modules" --exclude="/.git" --exclude="/.dat" --exclude="/datasets" ~/Sync/sign-search/* -e ssh web@auslan.fyi:/home/web/beta.find.auslan.fyi
+rsync -avr --delete-after --exclude=".DS_Store" --exclude="/node_modules" --exclude="/.git" --exclude="/.dat" --exclude="/datasets" ~/Sync/sign-search/* -e ssh web@auslan.fyi:/home/web/beta.find.auslan.fyi
