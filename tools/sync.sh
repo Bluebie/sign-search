32f68382fab2208c7c820b27f144264a4c597b2c
Phoenix Fox <Bluebie@users.noreply.github.com>
2020-02-13 18:04:12 +1100

    simplified and improved clarity of tools/sync.sh script

--- tools/sync.sh
+++ tools/sync.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-rsync -avr --delete-after web@auslan.fyi:/home/web/beta.find.auslan.fyi/logs -e ssh ~/Sync/sign-search
-rsync -avr --delete-after web@auslan.fyi:/home/web/beta.find.auslan.fyi/datasets -e ssh ~/Sync/sign-search
-rsync -avr --delete-after web@auslan.fyi:/home/web/beta.find.auslan.fyi/feeds -e ssh ~/Sync/sign-search
-rsync -avr --delete-after web@auslan.fyi:/home/web/beta.find.auslan.fyi/datasets.torrent -e ssh ~/Sync/sign-search
-rsync -avr --delete-after web@auslan.fyi:/home/web/beta.find.auslan.fyi/tools/spiders/frozen-data -e ssh ~/Sync/sign-search/tools/spiders
-rsync -avr --delete-after --exclude=".DS_Store" --exclude="/node_modules" --exclude="/tools/spiders/frozen-data" --exclude="/logs" --exclude="/datasets.torrent" --exclude="/datasets.torrent.*" --exclude="/.git" --exclude="/.dat" --exclude="/datasets" ~/Sync/sign-search/* -e ssh web@auslan.fyi:/home/web/beta.find.auslan.fyi
+rsync -avzr \
+  --include="/logs" \
+  --include="/datasets" \
+  --include="/feeds" \
+  --include="/datasets.torrent*" \
+  --include="/*.html*" \
+  --include="/tools/spiders/frozen-data" \
+  --exclude="*" -e ssh "web@auslan.fyi:/home/web/beta.find.auslan.fyi/*" ~/Sync/sign-search
+rsync -avzr --delete-after \
+  --exclude=".DS_Store" \
+  --exclude="/node_modules" \
+  --exclude="/*.html*" \
+  --exclude="/tools/spiders/frozen-data" \
+  --exclude="/logs" \
+  --exclude="/datasets.torrent*" \
+  --exclude="/.git" \
+  --exclude="/.dat" \
+  --exclude="/datasets" \
+  ~/Sync/sign-search/* -e ssh web@auslan.fyi:/home/web/beta.find.auslan.fyi
