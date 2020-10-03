7f9ee246abd0c323ede7c97c345057e8fd493640
Phoenix Fox <Bluebie@users.noreply.github.com>
2020-10-03 15:20:33 +1000

    /lib refactored to comply with Standard JS code style

--- server-side-cgi/search.js
+++ server-side-cgi/search.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 const URL = require('url').URL
-//const cgi = require('node-cgi')
 const process = require('process')
 const arp = require('app-root-path')
 const fs = require('fs-extra')
@@ -15,41 +14,41 @@ const SearchEngine = arp.require('lib/search-engine/engine')
 const signSearchConfig = arp.require('package.json').signSearch
 
 // local node digest function
-async function digest(algo, data) {
-  let hash = crypto.createHash(algo)
+async function digest (algo, data) {
+  const hash = crypto.createHash(algo)
   hash.update(data)
   return new Uint8Array(hash.digest())
 }
 
 // handle an incomming cgi request, making a similar interface to node's http server
-async function handleCGI(handler) {
-  let url = new URL(process.env.REQUEST_URI || `${process.env.SCRIPT_NAME}?${process.env.QUERY_STRING}`, `http://${process.env.HTTP_HOST || process.env.SERVER_NAME}/`)
+async function handleCGI (handler) {
+  const url = new URL(process.env.REQUEST_URI || `${process.env.SCRIPT_NAME}?${process.env.QUERY_STRING}`, `http://${process.env.HTTP_HOST || process.env.SERVER_NAME}/`)
   url.protocol = process.env.HTTPS ? 'https' : 'http'
-  let request = {
+  const request = {
     method: process.env.REQUEST_METHOD,
     headers: {},
-    url,
+    url
   }
 
-  Object.entries(process.env).forEach(([key, value])=> {
+  Object.entries(process.env).forEach(([key, value]) => {
     if (key.startsWith('HTTP_')) {
       request.headers[key.slice(5).toLowerCase().replace('_', '-')] = value
     }
   })
 
-  let respond = async (status, headers, body) => {
+  const respond = async (status, headers, body) => {
     if (!Buffer.isBuffer(body)) body = Buffer.from(body, 'utf-8')
-    let finalHeaders = {
-      'Status': status,
+    const finalHeaders = {
+      Status: status,
       'Content-Length': Buffer.byteLength(body),
       'Content-Type': 'text/plain; charset=utf-8',
-      ...headers,
+      ...headers
     }
 
-    let write = util.promisify(process.stdout.write).bind(process.stdout)
-    await write(Object.entries(finalHeaders).map(([key, value])=> {
+    const write = util.promisify(process.stdout.write).bind(process.stdout)
+    await write(Object.entries(finalHeaders).map(([key, value]) => {
       return `${key}: ${value}\n`
-    }).join("") + "\n")
+    }).join('') + '\n')
     await write(body)
   }
 
@@ -57,15 +56,15 @@ async function handleCGI(handler) {
 }
 
 // run the cgi-bin script
-async function handleRequest(request, respond) {
-  let document = new HTMLDocument({
+async function handleRequest (request, respond) {
+  const document = new HTMLDocument({
     ...signSearchConfig,
     disable: { javascript: true, analytics: true }
   })
 
   try {
     // initialise search engine
-    let engine = new SearchEngine({
+    const engine = new SearchEngine({
       vectorLibraryPath: arp.resolve('datasets/cc-en-300-8bit'),
       searchLibraryPath: arp.resolve('datasets/search-index'),
       libraryConfig: {
@@ -74,27 +73,26 @@ async function handleRequest(request, respond) {
     })
     // load any data needed to make it queryable
     await engine.load()
-    
-    // grab content from query string that we need
-    let query = document.query = request.url.searchParams.get('query') || ''
-    let offset = parseInt(request.url.searchParams.get('offset')) || 0
 
-    if (query.trim() == '') throw new Error("Please enter a search query");
+    // grab content from query string that we need
+    const query = document.query = request.url.searchParams.get('query') || ''
+    const offset = parseInt(request.url.searchParams.get('offset')) || 0
+
+    if (query.trim() === '') throw new Error('Please enter a search query')
 
     // set document query string to fill search box
-    let results = await engine.query(query)
-    
-    if (results.length == 0) throw new Error("No results found")
-    
+    const results = await engine.query(query)
+
+    if (results.length === 0) throw new Error('No results found')
+
     await document.setBody(new ResultsPage({ results, offset, query }))
   } catch (err) {
     await document.setBody(new ErrorPage(err.message))
   }
-  
+
   await respond(200, {
     'Content-Type': 'text/html; charset=utf-8'
   }, document.toHTML().toString())
 }
-
 
 handleCGI(handleRequest)
